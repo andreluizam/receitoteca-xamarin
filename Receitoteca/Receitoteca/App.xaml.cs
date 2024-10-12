7d8222d6bb3ec9b32e0982c57b47f3bd288fbe16
André Luiz <andreluizam2765@gmail.com>
2024-10-12 17:34:56 -0300

    Commit inicial

---
+++ Receitoteca/Receitoteca/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Receitoteca.Views;
+using Xamarin.Forms;
+
+namespace Receitoteca
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PaginaInicial();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
